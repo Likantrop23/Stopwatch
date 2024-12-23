19787b3b8ccc07d7075045acd7f8a87decb5f27d
Lichor <galunskiilya2301@gmail.com>
2024-12-23 16:53:37 +0700

    first commit

---
+++ app/src/main/java/com/Ilya/android/stopwatch/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.Ilya.android.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+
+class MainActivity : AppCompatActivity() {
+    lateinit var stopwatch: Chronometer // Хронометр
+    var running = false // Хронометр работает?
+    var offset: Long = 0 //Базовое смещение
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Получение ссылки на секундомер
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        //Восстановление предыдущего состояния
+        if (savedInstanceState != null) {
+            //Восстановить значения из объекта Bundle
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            //Установить время на секундомере и запустить его, если он должен  выполняться.
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
+
+        //Кнопка start запускает секундомер, если он не работал
+        val startButton = findViewById<Button>(R.id.start_button)
+
+        startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+        //Кнопка pause останавливает секундомер, если он работал
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+        //Кнопка reset обнуляет offset и базовое время
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    //когда активность переходит на задний план, смена фокуса на другое приложение
+    override fun onPause() {
+        super.onPause()
+        if (running) {
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+    //когда активность переходит на передний план
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            stopwatch.start()
+            offset = 0
+        }
+    }
+
+    //помещение значений в объект Bundle (сохранение значений при повороте экрана)
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    //Обновляет время stopwatch.base
+    fun setBaseTime() {
+        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+    //Сохраняет offset
+    fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+
+}
